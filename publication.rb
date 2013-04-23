4f84109fcc88c289ab110bc622be51b3536febed
Phil Gyford <phil@gyford.com>
2013-04-23 17:57:12 +0100

    Complete authentication: returns access_token and refresh_token

--- publication.rb
+++ publication.rb
@@ -4,14 +4,23 @@ require 'oauth2'
 require 'sinatra'
 require 'redis'
 
-client = OAuth2::Client.new(
+enable :sessions
+
+raise 'GOOGLE_CLIENT_ID is not set' if !ENV['GOOGLE_CLIENT_ID']
+raise 'GOOGLE_CLIENT_SECRET is not set' if !ENV['GOOGLE_CLIENT_SECRET']
+
+
+auth_client = OAuth2::Client.new(
   ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
-  :authorize_url => 'https://accounts.google.com/o/oauth2/auth',
-  :token_url => 'https://accounts.google.com/o/oauth2/token'
-})
+    :site => 'https://accounts.google.com',
+    :authorize_url => '/o/oauth2/auth',
+    :token_url => '/o/oauth2/token'
+  })
+
 
 configure do
   if settings.production?
+    raise 'REDISCLOUD_URL is not set' if !ENV['REDISCLOUD_URL']
     uri = URI.parse(ENV['REDISCLOUD_URL'])
     REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   else
@@ -33,6 +42,7 @@ configure do
   set :period, 'daily'
 end
 
+
 helpers do
   # So we know where to do redirects to.
   # Example return: 'http://my-app-name.herokuapp.com'
@@ -49,29 +59,63 @@ helpers do
   end
 end
 
+
+
 get %r{/(daily|weekly)/meta.json} do |period|
   set_period(period)
   content_type :json
   erb :meta
 end
 
+
 get %r{/(daily|weekly)/edition/} do |period|
   set_period(period)
   erb :publication
 end
 
+# == Parameters
+#   params['return_url'] will be the publication-specific URL we return the
+#     user to after authenticating.
+#
 get %r{/(daily|weekly)/configure/} do |period|
   set_period(period)
+  return 400, 'No return_url parameter was provided' if !params['return_url']
+
+  # Save the return URL so we still have it after authentication.
+  session[:bergcloud_return_url] = params['return_url']
+
+  begin
+    redirect auth_client.auth_code.authorize_url(
+      :scope => 'https://www.googleapis.com/auth/analytics.readonly',
+      :redirect_uri => "#{domain}/#{settings.period}/return/",
+      :access_type => 'offline',
+      :approval_prompt => 'force'
+    )
+  end
 end
+
 
 get %r{/(daily|weekly)/return/} do |period|
   set_period(period)
+
+  return 500, "No access token was returned by Google" if !params[:code]
+
+  access_token_obj = auth_client.auth_code.get_token(params[:code], {
+                      :redirect_uri => "#{domain}/#{settings.period}/return/",
+                      :token_method => :post
+                    })
+  @access_token = access_token_obj.token
+  @refresh_token = access_token_obj.refresh_token
+  
+  erb :publication
 end
+
 
 get %r{/(daily|weekly)/sample/} do |period|
   set_period(period)
   erb :publication
 end
+
 
 get %r{/(daily|weekly)/validate_config/} do |period|
   set_period(period)
