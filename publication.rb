38088edbb092a08798781db17ddd4efcfb248e2b
Phil Gyford <phil@gyford.com>
2013-04-23 14:29:14 +0100

    Add basic structure

---
+++ publication.rb
@@ -0,0 +1,64 @@
+# coding: utf-8
+require 'sinatra'
+require 'redis'
+
+configure do
+  if settings.production?
+    uri = URI.parse(ENV['REDISCLOUD_URL'])
+    REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  else
+    REDIS = Redis.new()
+  end
+
+  if settings.development?
+    # So we can see what's going wrong on Heroku.
+    set :show_exceptions, true
+  end
+
+  # The different periods we can display results for.
+  # The periods should be options within the URL route matching for each
+  # endpoint, eg:
+  #   get %r{/(daily|weekly)/edition/} do |period|
+  set :valid_periods, ['daily', 'weekly']
+  
+  # The default, which can be changed depending on URL.
+  set :period, 'daily'
+end
+
+helpers do
+  # So we know where to do redirects to.
+  # Example return: 'http://my-app-name.herokuapp.com'
+  # Should handle http/https and port numbers.
+  def domain
+    protocol = request.secure? ? 'https' : 'http'
+    port = request.env['SERVER_PORT'] ? ":#{request.env['SERVER_PORT']}" : ''
+    return "#{protocol}://#{request.env['SERVER_NAME']}#{port}"
+  end
+
+  # Set the `period` setting to `period` if it's valid. Else, ignore.
+  def set_period(period)
+    settings.period = period if settings.valid_periods.include?(period)
+  end
+end
+
+get %r{/(daily|weekly)/edition/} do |period|
+  set_period(period)
+  erb :publication
+end
+
+get %r{/(daily|weekly)/configure/} do |period|
+  set_period(period)
+end
+
+get %r{/(daily|weekly)/return/} do |period|
+  set_period(period)
+end
+
+get %r{/(daily|weekly)/sample/} do |period|
+  set_period(period)
+  erb :publication
+end
+
+get %r{/(daily|weekly)/validate_config/} do |period|
+  set_period(period)
+end
